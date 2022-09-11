529de753af8788d092748e81738dc6a30b97fb4d
Andrei Shumilov <andrei1shum@mail.ru>
2022-09-11 15:34:31 +0300

    Добавлено задание 4

---
+++ Epam Training/Lab_4/CNum.cs
@@ -0,0 +1,54 @@
+﻿
+
+namespace Lab_4
+{
+    public class CNum
+    {
+        public int _NumA { get; set; }
+        public int _NumB { get; set; }
+
+
+        public CNum() : this(0, 0) { }
+
+        public CNum(int a) : this(a, 0) { }
+
+        public CNum(int a, int b)
+        {
+            _NumA = a;
+            _NumB = b;
+        }
+
+
+        public string Print()
+        {
+            return $"{_NumA} + {_NumB}i";
+        }
+
+        public CNum Add(CNum secondCNum)
+        {
+            int newA = _NumA + secondCNum._NumA;
+            int newB = _NumB + secondCNum._NumB;
+
+            CNum result = new (newA, newB);
+            return result;
+        }
+
+        public CNum Subt(CNum secondCNum)
+        {
+            int newA = _NumA - secondCNum._NumA;
+            int newB = _NumB - secondCNum._NumB;
+
+            CNum result = new (newA, newB);
+            return result;
+        }
+
+        public CNum Mult(CNum secondCNum)
+        {
+            int newA = (_NumA * secondCNum._NumA) - (_NumB * secondCNum._NumB);
+            int newB = (_NumA * secondCNum._NumA) + (_NumB * secondCNum._NumB);
+
+            CNum result = new (newA, newB);
+            return result;
+        }
+    }
+}
---
+++ Epam Training/Lab_4/Program.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Lab_4
+{
+    class Program
+    {
+        public static void Input( int answersCounter, int maxIncorrectAnswers, out int a, out int b, string N)
+        {
+            a = 0;
+            b = 0;
+            while (answersCounter < maxIncorrectAnswers)
+            {
+                Console.Clear();
+                Console.WriteLine(" | после {0} некорректных ответов программа завершится |\n", maxIncorrectAnswers - answersCounter);
+                Console.WriteLine(" {0} комплексное число:", N);
+
+                try
+                {
+                    Console.WriteLine(" Введите а:");
+                    Console.CursorLeft = 1;
+                    a = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(" Введите b:");
+                    Console.CursorLeft = 1;
+                    b = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine(" | на ввод принимаются только целые числа |");
+                    answersCounter++;
+                    Console.WriteLine(" | Нажмите лювую клавишу чтобы продолжить |");
+                    Console.ReadKey();
+                    continue;
+                }
+            }
+        }
+
+        public static void PrintResult(CNum result)
+        {
+            Console.CursorLeft = 4;
+            Console.WriteLine(" Результат: {0}", result.Print());
+            Console.WriteLine("\n | Завершено. Нажмите лювую клавишу чтобы продолжить |");
+        }
+
+
+        static void Main()
+        {
+            int answer = 4;
+            int answersCounter = 0;
+            int maxIncorrectAnswers = 5;
+            int a;
+            int b;
+
+
+            Input(answersCounter, maxIncorrectAnswers, out a, out b, "Первое");
+            CNum cNumA = new(a, b);
+
+            Input(answersCounter, maxIncorrectAnswers, out a, out b, "Второе");
+            CNum cNumB = new(a, b);
+
+            CNum result;
+
+            while (answersCounter < maxIncorrectAnswers)
+            {
+                Console.Clear();
+                Console.WriteLine(" Первое комплексное число: {0}", cNumA.Print());
+                Console.WriteLine(" Второе комплексное число: {0}", cNumB.Print());
+                Console.WriteLine("\n Выберите действие:\n 1.Сложение\n 2.Вычитание\n 3.Умножение");
+                Console.WriteLine("\n Введите 0 чтобы завершить работу программы");
+                Console.WriteLine("\n | после {0} некорректных ответов программа завершится |\n", maxIncorrectAnswers - answersCounter);
+
+                try
+                {
+                    Console.CursorLeft = 1;
+                    answer = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine(" | на ввод принимаются только целые числа |");
+                    answersCounter++;
+                    Console.WriteLine(" | Нажмите лювую клавишу чтобы продолжить |");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                switch (answer)
+                {
+                    case 1:
+                        answersCounter = 0;
+                        Console.WriteLine(" | Сложение комплексных чисел |\n");
+                        result = cNumA.Add(cNumB);
+                        PrintResult(result);
+                        break;
+
+                    case 2:
+                        answersCounter = 0;
+                        Console.WriteLine(" | Вычитание комплексных чисел |\n");
+                        result = cNumA.Subt(cNumB);
+                        PrintResult(result);
+                        break;
+
+                    case 3:
+                        answersCounter = 0;
+                        Console.WriteLine(" | Умножение комплексных чисел |\n");
+                        result = cNumA.Mult(cNumB);
+                        PrintResult(result);
+                        break;
+
+                    case 0:
+                        return;
+
+                    default:
+                        Console.WriteLine(" | нужно ввести 1, 2 или 3 |");
+                        Console.WriteLine(" | Нажмите лювую клавишу чтобы продолжить |");
+                        answersCounter++;
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
